caa1c303806a9b1b39fc424f08916fd89e327f3e
Cynthia <CynthiaSpinner@gmail.com>
2024-04-11 10:36:01 -0500

    finished exercise 2

---
+++ FactoryPatternExercise2/ListDataAccess.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPatternExercise2
+{
+    public class ListDataAccess : IDataAccess
+    {
+        public static List<Product> Products = new List<Product>()
+        {
+            new Product() { Name = "Labtop", Price = 3000},
+                new Product() { Name = "TV", Price = 900},
+                    new Product() { Name = "Surround Sound", Price = 1000},
+                        new Product() { Name = "Studio Monitors", Price = 2000}
+        };
+
+        public List<Product> LoadData()
+        {
+            Console.WriteLine($"I am reading data from SQL Data Access");
+
+            return Products;
+
+        }
+
+        public void SaveData()
+        {
+            Console.WriteLine($"I am saving data from SQL Data Access ");
+        }
+    }
+}
--- FactoryPatternExercise2/Program.cs
+++ FactoryPatternExercise2/Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string userInput;
+            bool correctInput;
+            do {
+
+                correctInput = true;
+
+                Console.WriteLine("What database do your want to use? List, SQL or Mongo");
+
+                userInput = Console.ReadLine();
+
+                if(userInput != "mongo" && userInput != "sql" && userInput != "list")
+                {
+                    correctInput = false;
+                    Console.WriteLine("Try Again!");
+                    Thread.Sleep(1000);
+                }
+
+            } while (!correctInput); ;
+
+            Console.Clear();
+
+            IDataAccess data = DataAccessFactory.GetDataAccessType(userInput);
+
+            var products = data.LoadData();
+
+            data.SaveData();
+
+            foreach(var product in products) 
+            {
+                Console.WriteLine($"Product: {product.Name} Price: {product.Price}");
+            }
         }
     }
 }
